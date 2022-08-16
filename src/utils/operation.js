18ed2c2d5320cbce6dd0836eb284f5b0861fab73
NAVYA S RAJ <84143583+navyasraj02@users.noreply.github.com>
2022-08-16 16:36:20 +0530

    Updated frontend

--- src/utils/operation.js
+++ src/utils/operation.js
@@ -1,10 +1,11 @@
 // TODO 6 - Call buy_ticket entrypoint in the Lottery contract
 import { tezos } from "./tezos";
 
-export const giveFundOperation = async (from_uuid,from_address,to_puid,amount,comment) => {
+export const giveFundOperation = async (transid,from_uuid,from_address,to_puid,amount,comment) => {
   try {
+    transid = transid + 1;
     const contractInstance = await tezos.wallet.at("KT1AdnJw5wbdH2o3f3mBcamgmd1t7hhKSWqA");
-    const op = await contractInstance.methods.add_transaction1(from_uuid,from_address,to_puid,amount,comment).send({
+    const op = await contractInstance.methods.add_transaction1(String(transid),from_uuid,from_address,to_puid,amount,comment).send({
       amount: amount,
       mutez: false,
     });
@@ -21,7 +22,7 @@ export const addNewUseraoperation = async (uuid,email) => {
     
     try {
     const contractInstance = await tezos.wallet.at("KT1AdnJw5wbdH2o3f3mBcamgmd1t7hhKSWqA");
-    contractInstance.methods.add_user(uuid,email)
+    contractInstance.methods.add_user(email,uuid)
     } catch (err) {
       throw err;
     }
@@ -30,12 +31,12 @@ export const addNewUseraoperation = async (uuid,email) => {
 
 // add profile
 
-export const addprofileOperation= async (uuid,email) => {
+export const addprofileOperation= async (name, desc, comp, comp_t, ppt, video, puuid, puid, goal, dl, addr, pic) => {
   
   
   try {
     const contractInstance = await tezos.wallet.at("KT1AdnJw5wbdH2o3f3mBcamgmd1t7hhKSWqA");
-   contractInstance.methods.add_applic(uuid,email);
+   contractInstance.methods.add_applic(name, desc, comp, comp_t, ppt, video, puuid, puid, goal, dl, addr, pic);
     } catch (err) {
       throw err;
     }
